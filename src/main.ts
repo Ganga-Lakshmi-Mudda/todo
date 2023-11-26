f3dae30a463629758d63947e23bd0d3c99e0478b
Ganga-Lakshmi-Mudda <gangajyothi333@gmail.com>
2023-11-26 12:01:44 +0530

    backend global prefix api added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors();
   const port = 3000 || 4200;
   await app.listen(port);
